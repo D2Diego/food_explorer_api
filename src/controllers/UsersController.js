09a21b933318784817fa7e180ae7423e16a08208
diego <dmarques1508@gmail.com>
2024-02-16 14:29:14 -0300

    Atualizando senha

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,5 +1,5 @@
 const AppError = require('../utils/AppError');
-const { hash } = require('bcryptjs')
+const { hash, compare } = require('bcryptjs')
 const knex = require('../database/knex');
 
 class UsersController {
@@ -24,7 +24,7 @@ class UsersController {
   }
 
   async update(request, response){
-    const { name, email} = request.body;
+    const { name, email, password, old_password} = request.body;
     const { id } = request.params
 
     const user = await knex('users').where({ id: id }).first();
@@ -42,12 +42,26 @@ class UsersController {
     user.name = name;
     user.email = email
 
+    if(password && !old_password){
+      throw new AppError('Você precisa digitar a senha antiga')
+    }
+
+    if(password && old_password){
+      const chekOldPassword = await compare(old_password, user.password)
+
+      if(!chekOldPassword){
+        throw new AppError('A senha antiga não confere')
+      }
+
+      user.password = await hash(password, 9)
+    }
+
     await knex('users').where({ id : id }).update({
       name: name,
       email: email,
-    }).finally(() => {
-        knex.destroy()
-      });
+      password: user.password,
+      updated_at: knex.fn.now()
+      }).finally(() => {knex.destroy() });
 
       return response.json();
   }
