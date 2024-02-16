6454c18f5d995a72ee7bf16882efcb798e2070f0
diego <dmarques1508@gmail.com>
2024-02-15 23:28:58 -0300

    Fix UsersController

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,13 +1,11 @@
 const AppError = require('../utils/AppError');
 
-const sqliteConnection = require("../database/sqlite");
 const knex = require('../database/knex');
 
 class UsersController {
   async create(request, response) {
    const { name, email, password } = request.body;
 
-   const database = await sqliteConnection();
    const checkUsersExists = await knex('users').where({ email: email }).select('*')
 
    if (checkUsersExists) {
