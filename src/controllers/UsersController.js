7add3f0669de6e73e70d2a2c0b3826f71f0a6106
diego <dmarques1508@gmail.com>
2024-02-16 13:43:15 -0300

    Criando usuário e encripitando a senha

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,16 +1,24 @@
 const AppError = require('../utils/AppError');
-
+const { hash } = require('bcryptjs')
 const knex = require('../database/knex');
 
 class UsersController {
   async create(request, response) {
    const { name, email, password } = request.body;
 
-   const checkUsersExists = await knex('users').where({ email: email }).select('*')
+   const checkUsersExists = await knex('users').where({ email: email }).first();
 
    if (checkUsersExists) {
     throw new AppError('Este email já está em uso')
   }
+
+
+    const hashedPassword = await hash(password, 9)
+  await knex('users').insert({
+    name: name,
+    email: email,
+    password: hashedPassword,
+  });
 
     return response.status(201).json()
   }
