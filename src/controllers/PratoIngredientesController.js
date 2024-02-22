7914216faf1e0accaebb7818e3ced16d89f27c13
diego <dmarques1508@gmail.com>
2024-02-21 22:52:08 -0300

    feat: delete from PratoIngrediente

--- src/controllers/PratoIngredientesController.js
+++ src/controllers/PratoIngredientesController.js
@@ -1,3 +1,4 @@
+const { request, response } = require('express');
 const knex = require('../database/knex');
 const AppError = require('../utils/AppError');
 
@@ -23,6 +24,32 @@ class PratoIngredientesController{
       
         return response.status(201).json({ message: 'Ingredientes adicionados ao prato com sucesso.' });
       }
-}
 
+      async delete(request, response) {
+        const { prato_id, ingrediente_ids } = request.body;
+      
+        // Verifica se prato_id ou ingrediente_ids não estão definidos ou se ingrediente_ids está vazio
+        if (!prato_id || !ingrediente_ids || ingrediente_ids.length === 0) {
+          return response.status(400).json({ message: 'O prato_id e ingrediente_ids são necessários e não podem ser vazios.' });
+        }
+      
+        try {
+          const deleted = await knex('prato_ingredientes')
+            .where({ prato_id })
+            .whereIn('ingrediente_id', ingrediente_ids)
+            .del();
+      
+          if (deleted) {
+            return response.status(200).json({ message: 'Ingredientes removidos do prato com sucesso!' });
+          } else {
+            // Se nenhum registro foi deletado, possivelmente não foi encontrado
+            return response.status(404).json({ message: 'Relacionamento prato-ingrediente não encontrado' });
+          }
+        } catch (error) {
+          console.error(error);
+          return response.status(500).json({ message: 'Erro ao tentar deletar ingredientes do prato.' });
+        }
+      }
+}
+ 
 module.exports = PratoIngredientesController;
--- src/routes/Pratoingredientes.routes.js
+++ src/routes/Pratoingredientes.routes.js
@@ -7,5 +7,6 @@ const pratoIngredientesRoutes = Router();
 const pratoIngredientesController = new PratoIngredientesController();
 
 pratoIngredientesRoutes.get('/', pratoIngredientesController.create);
+pratoIngredientesRoutes.delete('/', pratoIngredientesController.delete);
 
 module.exports = pratoIngredientesRoutes;
