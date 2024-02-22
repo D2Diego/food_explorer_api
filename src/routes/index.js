86700ea6179785221d5475271c3c560892bf659a
diego <dmarques1508@gmail.com>
2024-02-22 00:10:22 -0300

    feat: Rotas para sessions

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ const categoriasRoutes = require('./categorias.routes')
 const pratosRoutes = require('./pratos.routes')
 const ingredientesRoutes = require('./ingredientes.routes')
 const pratoIngredientesRoutes = require('./Pratoingredientes.routes')
+const sessionsRouter = require('./Sessions.routes')
 
 const routes = Router()
 routes.use('/users', usersRoutes)
@@ -12,6 +13,7 @@ routes.use('/categorias', categoriasRoutes)
 routes.use('/pratos', pratosRoutes)
 routes.use('/ingredientes', ingredientesRoutes)
 routes.use('/pratoingredientes', pratoIngredientesRoutes)
+routes.use('/sessions', sessionsRouter)
 
 
 module.exports = routes
