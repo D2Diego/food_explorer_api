1a6ae4623da4bf2da4bb39de6e8caf594af0321e
diego <dmarques1508@gmail.com>
2024-02-22 23:31:22 -0300

    fix: pratoAvatarController.js

--- src/routes/pratos.routes.js
+++ src/routes/pratos.routes.js
@@ -3,6 +3,7 @@ const multer = require("multer")
 const uploadConfig  = require('../config/upload')
 
 const PratosController = require('../controllers/PratosController');
+const PratoAvatarController = require('../controllers/PratoAvatarController');
 
 const ensureAutheticated = require('../middlewares/ensureAutheticated');
 const isAdmin = require('../middlewares/isAdmin');
@@ -11,15 +12,12 @@ const pratosRoutes = Router();
 const upload = multer(uploadConfig.MULTER)
 
 const pratosController = new PratosController();
+const pratoAvatarController = new PratoAvatarController();
 
 pratosRoutes.post('/',ensureAutheticated, isAdmin, pratosController.create);
 pratosRoutes.put('/:id',ensureAutheticated, isAdmin, pratosController.updated);
 pratosRoutes.get('/:id',ensureAutheticated, pratosController.read);
 pratosRoutes.delete('/:id',ensureAutheticated, isAdmin, pratosController.delete);
-pratosRoutes.patch('/avatar',ensureAutheticated, isAdmin, upload.single("avatar"),
-(request, response) => {console.log(request.file.filename); 
-    response.json();
-
-});
+pratosRoutes.patch('/:id/avatar',ensureAutheticated, isAdmin, upload.single("avatar"), pratoAvatarController.update );
 
 module.exports = pratosRoutes;
