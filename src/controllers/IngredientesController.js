f40b7af7b83c247f682d198f078d642856e3eae6
diego <dmarques1508@gmail.com>
2024-03-02 20:47:41 -0300

    fix: corrigindo create do IngredientesController.js

--- src/controllers/IngredientesController.js
+++ src/controllers/IngredientesController.js
@@ -1,39 +1,44 @@
 const knex = require('../database/knex');
 const AppError = require('../utils/AppError');
 
-class IngredientesController{
-    async create(request, response){
-        const { nome } = request.body;
+class IngredientesController {
+    async create(request, response) {
+        const { nomes } = request.body; 
 
-        if(!nome){
-            throw new AppError('O nome é obrigatório.', 400)
+        if (!nomes || nomes.length === 0) {
+            throw new AppError('Ao menos um nome é obrigatório.', 400);
         }
 
-        const checkIngredientExists = await knex('ingredientes').where({ nome }).first();
+        const ingredientesInseridos = [];
 
-        if(checkIngredientExists){
-            throw new AppError('Esse ingrediente já existe', 400)
-        }
+        for (const nome of nomes) {
+            if (!nome) continue;
 
-        await knex('ingredientes').insert({
-            nome
-        })
+            const checkIngredientExists = await knex('ingredientes').where({ nome }).first();
 
-        return response.status(201).json({message: 'Ingediente criado com sucesso'});
-    }
-
-        async delete(request, response){
-            const { id } = request.params;
-
-            const deleted = await knex('ingredientes').where({ id: id }).del();
-
-            if(deleted){
-                response.status(200).json({ message: 'Ingrediente excluido com sucesso'})
+            if (!checkIngredientExists) {
+                
+                const [ingredienteId] = await knex('ingredientes').insert({ nome }).returning('id');
+                ingredientesInseridos.push({ id: ingredienteId, nome });
             } else {
-                response.status(404).json({ message: 'Ingrediente não encontrado'})
+                console.log(`Ingrediente '${nome}' já existe e foi ignorado.`);
             }
         }
 
+        return response.status(201).json({ message: 'Ingredientes criados com sucesso', ingredientes: ingredientesInseridos });
+    }
+
+    async delete(request, response) {
+        const { id } = request.params;
+
+        const deleted = await knex('ingredientes').where({ id }).del();
+
+        if (deleted) {
+            response.status(200).json({ message: 'Ingrediente excluído com sucesso' });
+        } else {
+            response.status(404).json({ message: 'Ingrediente não encontrado' });
+        }
+    }
 }
 
 module.exports = IngredientesController;
