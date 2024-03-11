d3fca19fdba3497a474930808290d89c4ee52209
D2Diego <dmarques1508@gmail.com>
2024-03-11 17:01:27 -0300

    refactory: SessionsController

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,15 +1,21 @@
+// Knex, App Error, Compare, Token and AuthConfig Import
 const knex = require("../database/knex");
+
 const authConfig = require("../configs/auth");
 const { sign } = require("jsonwebtoken");
+
 const AppError = require("../utils/AppError");
 const { compare } = require("bcryptjs");
 
 class SessionsController {
     async create(request, response) {
+        // Capturing Body Parameters
         const { email, password } = request.body;
 
+        // Getting the user data through the informed e-mail
         const user = await knex("users").where({ email }).first();
 
+        // Verifications
         if (!user) {
             throw new AppError("E-mail e/ou senha incorretos", 401);
         }
@@ -23,12 +29,10 @@ class SessionsController {
         const { secret, expiresIn } = authConfig.jwt;
         const token = sign({}, secret, {
             subject: String(user.id),
-            expiresIn,
-        });
+            expiresIn
+        })
 
-        const { password: _, ...userDataWithoutPassword } = user;
-
-        return response.status(201).json({ user: userDataWithoutPassword, token });
+        return response.status(201).json({ user, token });
     }
 }
 
