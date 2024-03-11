a049ed4562e1b1668b080bcf590cf251c2c8fe46
D2Diego <dmarques1508@gmail.com>
2024-03-11 20:58:27 -0300

    feat: tentando caminhos

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,9 @@
+const caminhoServer = require("./src/server");
+
 module.exports = {
   apps : [{
     name: "app",
-    script: "./src/server",
+    script: caminhoServer,
     instances: "max",
     env: {
       NODE_ENV: "development",
@@ -11,5 +13,3 @@ module.exports = {
     }
   }]
 }
-
-const teste = require("./src/server")
